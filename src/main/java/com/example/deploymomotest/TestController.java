6ab0183ebef6833a8e781f036e8cef1999d6b453
gangdodan <dodannnn4@gmail.com>
2022-12-31 01:44:52 +0900

    안되는거2

--- src/main/java/com/example/deploymomotest/TestController.java
+++ src/main/java/com/example/deploymomotest/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String healthCheck(){
-        return "안되는거 : 자동 빌드, 도커허브에서 이미지 Pull";
+        return "안되는거 : 도커허브에서 이미지 Pull";
     }
 }
